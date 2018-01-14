ca6ee2e514fdaec981f93dbc4511953f2410d23e
nicholasbair <bairinc@gmail.com>
2018-01-13 17:11:41 -0700

    Fix params type issue

--- actions/scrape.go
+++ actions/scrape.go
@@ -1,13 +1,22 @@
 package actions
 
 import (
+	"net/url"
+
 	"github.com/gobuffalo/buffalo"
 	"github.com/nicholasbair/goscraper"
 )
 
 // ScrapeHandler is a handler for scraping and stuff
 func ScrapeHandler(c buffalo.Context) error {
-	return c.Render(200, r.JSON(goscraper.Scrape(c.Params())))
+	p := map[string][]string{}
+
+	if m, ok := c.Params().(url.Values); ok {
+		for k, v := range m {
+			p[k] = v
+		}
+	}
+	return c.Render(200, r.JSON(goscraper.Scrape(p)))
 }
 
 // GetAllScrapersHandler is a handler for returning available scrapers
